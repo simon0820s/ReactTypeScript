693472b382204c4c9d18251d7d69d5db09fdc71a
simon0820s <simon0820s@gmail.com>
2023-06-01 12:54:59 -0500

    add general types

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,34 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 interface Sub {
   nick: string,
   subMonths: number,
   avatar: string,
-  description?: string 
+  description?: string
 }
 
+const INITIAL_STATE = [
+  {
+    nick: 'name',
+    subMonths: 3,
+    avatar: 'https://i.pravatar.cc/150?u=dapelu',
+    description: 'Dapelu hace de moderador'
+  },
+  {
+    nick: 'sergio_serrano',
+    subMonths: 7,
+    avatar: 'https://i.pravatar.cc/150?u=sergio_serrano'
+  }
+]
+
 function App() {
-  const [subs, setSubs] = useState([
-    {
-      nick: 'name',
-      subMonths: 3,
-      avatar: 'https://i.pravatar.cc/150?u=dapelu',
-      description: 'Dapelu hace de moderador'
-    },
-    {
-      nick: 'sergio_serrano',
-      subMonths: 7,
-      avatar: 'https://i.pravatar.cc/150?u=sergio_serrano'
-    }
-  ])
+  const [subs, setSubs] = useState<Array<Sub>>([])
+
+  useEffect(() => {
+    setSubs(INITIAL_STATE)
+  }, [])
+
   return (
     <div className='app'>
       <h1>Midu subs</h1>
